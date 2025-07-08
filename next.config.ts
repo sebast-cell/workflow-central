48a216990ddfad491b7d8a0d229905073cf17624
Sebastian Nuñez lozano <sebastian@confortsistemas.com>
2025-07-08 12:43:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++
@@ -1,4 +0,0 @@
-// This file has been renamed to next.config.js to fix a server start-up issue.
-// The TypeScript version of the config file was not supported in the execution environment.
-// This file can be safely deleted in the future.
-export {};
